49e6c9d6a1df131a1010ca86f3de9584d2fad46d
ahmtcntrn <ahmtcntrn@hotmail.com>
2023-02-27 22:04:34 +0300

    up

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { AxiosService } from './services/axios.services';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) { }
 
   @Get()
-  async getHello(): Promise<object> {
-    // axiosserviceyi çalıştır
-    const axiosService = new AxiosService();
-    return (await axiosService.getArticles('2020/10/01'));
+  async getHello(): Promise<string> {
+return "Hello"
   }
 }
